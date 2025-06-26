9514ee1e21b12eb2201c906b398df58f97e1d5d5
jaca230 <jaca230@uky.edu>
2025-06-26 00:59:47 -0400

    fixed double include in lindef

--- include/stages/LinkDef.h
+++ include/stages/LinkDef.h
@@ -7,7 +7,6 @@
 #pragma link C++ class BaseStage+;
 #pragma link C++ class DummyStage+;
 #pragma link C++ class BaseInputStage+;
-#pragma link C++ class BaseInputStage+;
 #pragma link C++ class RandomDataGeneratorStage+;
 #pragma link C++ class TH1BuilderStage+;
 #pragma link C++ class ClearProductsStage+;
